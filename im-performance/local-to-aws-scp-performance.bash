2dae3ba4ead55cc76ca5d911bc28cc6b4d04d6ec
Torben Hansen <Torben.Hansen.2015@rhul.ac.uk>
2017-05-23 16:06:18 +0100

    Updated test script

--- im-performance/local-to-aws-scp-performance.bash
+++ im-performance/local-to-aws-scp-performance.bash
@@ -2,7 +2,9 @@
 
 set -o pipefail
 
-BYTES=1MB
+BYTES=10MB
+SRC_HOST=local_rhul
+DST_HOST=aws_london
 HOST=ec2-52-56-140-16.eu-west-2.compute.amazonaws.com
 PORT=22221
 ID=aws_london
@@ -16,6 +18,13 @@ DATE=`date +%Y-%m-%d:%H:%M:%S`
 LOG_FILE_NAME=$DATE\_scp.log
 GREP_SCP='Bytes per second\|Bytes encrypted sent\|Bytes raw sent'
 REMOTE_USER=ubuntu
+CIPHER_SUITES=("aes128-ctr" "hmac-md5" "aes128-ctr" "hmac-md5-etm@openssh.com" "aes128-ctr" "umac-64-etm@openssh.com" "aes128-ctr" 
+	"hmac-sha1" "3des-cbc" "hmac-md5" "aes256-ctr" "hmac-sha2-512" "aes128-cbc" "hmac-sha1" "aes128-ctr" "hmac-ripemd160")
+AUTH_CIPHER_SUITES=("chacha20-poly1305@openssh.com" "aes128-gcm@openssh.com")
+INTERMAC_CIPHER_SUITES=("im-aes128-gcm-128" "im-chacha-poly-128" "im-aes128-gcm-256" "im-chacha-poly-256" "im-aes128-gcm-512"
+	"im-chacha-poly-512" "im-aes128-gcm-1024" "im-chacha-poly-1024" "im-aes128-gcm-2048" "im-chacha-poly-2048"
+	"im-aes128-gcm-4096" "im-chacha-poly-4096")
+
 
 rm_at_remote () {
 
@@ -46,7 +55,8 @@ echo ""
 echo "-----SCP BENCHMARK START-----"
 echo ""
 
-echo $HOST >> $LOG_FILE_NAME
+echo $SRC_HOST >> $LOG_FILE_NAME
+echo $DST_HOST >> $LOG_FILE_NAME
 echo $BYTES >> $LOG_FILE_NAME
 
 echo "Constructing temp data file:"
@@ -58,97 +68,26 @@ echo ""
 echo "Executing SCP using cipher suite"
 echo ""
 
-scp_cipher_mac "aes128-ctr" "hmac-md5"
+# "normal" ciphers suites
+for ((i=0; i<${#CIPHER_SUITES[@]}; i+=2));
+do
+	scp_cipher_mac "${CIPHER_SUITES[i]}" "${CIPHER_SUITES[i+1]}"
+	rm_at_remote
+done
 
-rm_at_remote
+# AE cipher suites
+for ((i=0; i<${#AUTH_CIPHER_SUITES[@]}; i+=1));
+do
+	scp_auth_cipher "${AUTH_CIPHER_SUITES[i]}"
+	rm_at_remote
+done
 
-scp_cipher_mac "aes128-ctr" "hmac-md5-etm@openssh.com"
-
-rm_at_remote
-
-scp_cipher_mac "aes128-ctr" "umac-64-etm@openssh.com"
-
-rm_at_remote
-
-scp_cipher_mac "aes128-cbc" "hmac-md5"
-
-rm_at_remote
-
-scp_auth_cipher "chacha20-poly1305@openssh.com"
-
-rm_at_remote
-
-scp_cipher_mac "aes128-ctr" "hmac-sha1"
-
-rm_at_remote
-
-scp_cipher_mac "3des-cbc" "hmac-md5"
-
-rm_at_remote
-
-scp_auth_cipher "aes128-gcm@openssh.com"
-
-rm_at_remote
-
-scp_cipher_mac "aes256-ctr" "hmac-sha2-512"
-
-rm_at_remote
-
-scp_cipher_mac "aes128-cbc" "hmac-sha1"
-
-rm_at_remote
-
-scp_cipher_mac "aes128-ctr" "hmac-ripemd160"
-
-rm_at_remote
-
-scp_auth_cipher "im-aes128-gcm-128"
-
-rm_at_remote
-
-scp_auth_cipher "im-chacha-poly-128"
-
-rm_at_remote
-
-scp_auth_cipher "im-aes128-gcm-256"
-
-rm_at_remote
-
-scp_auth_cipher "im-chacha-poly-256"
-
-rm_at_remote
-
-scp_auth_cipher "im-aes128-gcm-512"
-
-rm_at_remote
-
-scp_auth_cipher "im-chacha-poly-512"
-
-rm_at_remote
-
-scp_auth_cipher "im-aes128-gcm-1024"
-
-rm_at_remote
-
-scp_auth_cipher "im-chacha-poly-1024"
-
-rm_at_remote
-
-scp_auth_cipher "im-aes128-gcm-2048"
-
-rm_at_remote
-
-scp_auth_cipher "im-chacha-poly-2048"
-
-rm_at_remote
-
-scp_auth_cipher "im-aes128-gcm-4096"
-
-rm_at_remote
-
-scp_auth_cipher "im-chacha-poly-4096"
-
-rm_at_remote
+# InterMAC cipher suites
+for ((i=0; i<${#INTERMAC_CIPHER_SUITES[@]}; i+=1));
+do
+	scp_auth_cipher "${INTERMAC_CIPHER_SUITES[i]}"
+	rm_at_remote
+done
 
 rm $DATA_FILE_NAME
 
