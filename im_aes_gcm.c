245eec91bb35f1461d4ea9d1d42968e4a540f946
Torben Hansen <Torben.Hansen.2015@rhul.ac.uk>
2018-06-17 23:31:02 +0100

    Modify aes-gcm to output IM return codes

--- im_aes_gcm.c
+++ im_aes_gcm.c
@@ -9,52 +9,75 @@
 
 #include <stdio.h>
 
-int im_aes_gcm_init(struct im_cipher_st_ctx *im_cs_ctx, const u_char *key, u_int key_len, u_char *nonce, int crypt_type) {
+#include "im_common.h"
 
+int im_aes_gcm_init(struct im_cipher_st_ctx *im_cs_ctx, const u_char *key,
+	u_int key_len, u_char *nonce, int crypt_type) {
 
-	if ((im_cs_ctx->evp = EVP_CIPHER_CTX_new()) == NULL)
-		return -1;
+	EVP_CIPHER_CTX *_evp = NULL;
+	int r = IM_ERR;
+
+	if ((_evp = EVP_CIPHER_CTX_new()) == NULL) {
+		goto out;
+	}
 
 	/* Pick cipher (EVP_aes_128_gcm) and set key */
-	if (EVP_CipherInit(im_cs_ctx->evp, EVP_aes_128_gcm(), key, NULL, crypt_type) == 0)
-		return -1;
+	if (EVP_CipherInit(_evp, EVP_aes_128_gcm(), key, NULL, crypt_type) == 0) {
+		goto out;
+	}
 
+	im_cs_ctx->evp = _evp;
+	_evp = NULL;
 	im_cs_ctx->crypt_type = crypt_type;
 
-	return 0;
+	r = IM_OK;
+
+out:
+	if (_evp != NULL) {
+		EVP_CIPHER_CTX_free(_evp);
+	}
+	
+	return r;
 }
 
-int im_aes_gcm_cipher(struct im_cipher_st_ctx *im_cs_ctx, u_char *nonce, u_char *dst, const u_char *src, u_int src_length) {
+int im_aes_gcm_cipher(struct im_cipher_st_ctx *im_cs_ctx, u_char *nonce,
+	u_char *dst, const u_char *src, u_int src_length) {
 
 	int crypt_type = im_cs_ctx->crypt_type;
+	int r = IM_ERR;
 
-	if (crypt_type == IM_CIPHER_DECRYPT) {
+	if (IM_CIPHER_DECRYPT == crypt_type) {
 
 		/* Sets the MAC tag */
-		if(EVP_CIPHER_CTX_ctrl(im_cs_ctx->evp, EVP_CTRL_GCM_SET_TAG, 16, (u_char *) src + src_length) == 0)
-			return -1;
+		if(EVP_CIPHER_CTX_ctrl(im_cs_ctx->evp, EVP_CTRL_GCM_SET_TAG, 16,
+			(u_char *) src + src_length) == 0)
+			goto out;
 	}
 
 	/* Set new nonce */
 	if (EVP_CipherInit(im_cs_ctx->evp, NULL, NULL, nonce, crypt_type) == 0)
-		return -1;
+		goto out;
 
 	/* Encrypt/decrypt */
 	if (EVP_Cipher(im_cs_ctx->evp, dst, src, src_length) < 0) 
-		return -1;
+		goto out;
 
 	/* Verify (on derypt) or compute (on encrypt) MAC tag */
 	if (EVP_Cipher(im_cs_ctx->evp, NULL, NULL, 0) < 0)
-		return -1;
+		goto out;
 	
-	if (crypt_type == IM_CIPHER_ENCRYPT) {
+	if (IM_CIPHER_ENCRYPT == crypt_type) {
 
 		/* Set tag */
-		if (EVP_CIPHER_CTX_ctrl(im_cs_ctx->evp, EVP_CTRL_GCM_GET_TAG, 16, dst + src_length) == 0)
-			return -1;
-	}	
+		if (EVP_CIPHER_CTX_ctrl(im_cs_ctx->evp, EVP_CTRL_GCM_GET_TAG, 16,
+			dst + src_length) == 0)
+			goto out;
+	}
 
-	return 0;
+	r = IM_OK;
+
+out:
+	return r;
 }
 
 void im_aes_gcm_cleanup(struct im_cipher_st_ctx *im_cs_ctx) {
