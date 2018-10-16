7d88216bd4cdd5305641fb3957e0b678c5dc183f
Torben Hansen <Torben.Hansen.2015@rhul.ac.uk>
2018-10-16 17:48:04 +0100

    Updates to log parsing

--- im-performance/im_bench_parse_logs_new.py
+++ im-performance/im_bench_parse_logs_new.py
@@ -133,7 +133,7 @@ def draw_graph(ax, ylabels, data1, data2, msg_length, max_x_label):
 
 	ax.set_title('{}'.format(msg_length))
 
-	ax.set_yticks(y + height / 3)
+	ax.set_yticks(y + height / 2)
 	ax.set_yticklabels(ylabels)
 	ax.set_ylabel('chunk length')
 	
@@ -141,7 +141,33 @@ def draw_graph(ax, ylabels, data1, data2, msg_length, max_x_label):
 	ax.set_xlim(0, max_x_label)
 	ax.set_ylim(-0.5,14)
 
-	ax.legend((rec2[0], rec1[0]), ('chaCha20-poly1305','aes128-gcm'), loc='center right', prop={'size': 10})
+	ax.legend((rec2[0], rec1[0]), ('chacha20-poly1305','aes128-gcm'), loc='center right', prop={'size': 8})
+	
+	ax.xaxis.grid(color='green', linestyle='-')
+
+	#for r in rec:
+	#	w = r.get_width()
+	#	if not w == 0:	
+	#		ax.text(10, r.get_y() + 0.5, '{}'.format(w), color='blue', fontweight='bold')
+
+def draw_graph_only_aes(ax, ylabels, data, msg_length, max_x_label):
+
+	y = np.arange(len(ylabels))
+	height = 0.35
+
+	rec1 = ax.barh(y, data, height, align='center', color='red', label='aes128-gcm')
+
+	ax.set_title('{}'.format(msg_length))
+
+	ax.set_yticks(y)
+	ax.set_yticklabels(ylabels)
+	ax.set_ylabel('chunk length')
+	
+	ax.set_xlabel('cycles / byte')
+	ax.set_xlim(0, max_x_label)
+	ax.set_ylim(-0.5,14-0.5)
+
+	ax.legend(loc='center right', prop={'size': 8})
 	
 	ax.xaxis.grid(color='green', linestyle='-')
 
@@ -163,25 +189,19 @@ def do_graphs_grid(str_select):
 	if (str_select == 'encrypt'):
 		data1 = encrypt_aes128_gcm
 		data2 = encrypt_chacha_poly
-		# For both ciphers
-		#max_x_label_onekb = 150
-		#max_x_label_eightkb = 40
-		#max_x_label_fifteenkb = 40
-		#max_x_label_fiftykb = 40
-		# For aes128-gcm only
-		max_x_label_onekb = 4
-		max_x_label_eightkb = 4
-		max_x_label_fifteenkb = 4
-		max_x_label_fiftykb = 4
+		max_x_label_onekb = 150
+		max_x_label_eightkb = 40
+		max_x_label_fifteenkb = 40
+		max_x_label_fiftykb = 40
 	elif (str_select == 'decrypt'):
 		data1 = decrypt_aes128_gcm
 		data2 = decrypt_chacha_poly
-		max_x_label_onekb = 70
-		max_x_label_eightkb = 70
-		max_x_label_fifteenkb = 70
-		max_x_label_fiftykb = 70
+		max_x_label_onekb = 190
+		max_x_label_eightkb = 50
+		max_x_label_fifteenkb = 50
+		max_x_label_fiftykb = 50
 
-	fig = plt.figure(figsize=(10,10))
+	fig = plt.figure(figsize=(9,9))
 
 	fig.suptitle('{}()'.format(str_select), fontsize=22)
 
@@ -223,9 +243,72 @@ def do_graphs_grid(str_select):
 	plt.tight_layout(pad=1, w_pad=1, h_pad=1, rect=[0, 0, 1, 0.97])
 	plt.show()
 
+def do_graphs_grid_only_aes(str_select):
+
+	data1 = None
+	data2 = None
+
+	onekb = msg_sizes[0]
+	eightkb = msg_sizes[1]
+	fifteenkb = msg_sizes[2]
+	fiftykb = msg_sizes[3]
+
+	if (str_select == 'encrypt'):
+		data = encrypt_aes128_gcm
+		max_x_label_onekb = 5
+		max_x_label_eightkb = 3
+		max_x_label_fifteenkb = 3
+		max_x_label_fiftykb = 3
+	elif (str_select == 'decrypt'):
+		data = decrypt_aes128_gcm
+		max_x_label_onekb = 40
+		max_x_label_eightkb = 10
+		max_x_label_fifteenkb = 8
+		max_x_label_fiftykb = 8
+
+	fig = plt.figure(figsize=(9,9))
+
+	fig.suptitle('{}()'.format(str_select), fontsize=22)
+
+	gs = gridspec.GridSpec(2, 2, width_ratios=[1, 1], height_ratios=[1,1])
+	ax1 = plt.subplot(gs[0])
+	ax2 = plt.subplot(gs[1])
+	ax3 = plt.subplot(gs[2])
+	ax4 = plt.subplot(gs[3])
+	
+	draw_graph_only_aes(
+		ax1,
+		chunk_lengths,
+		cycles_per_byte(data[0], onekb),
+		'1kb',
+		max_x_label_onekb)
+	draw_graph_only_aes(
+		ax2,
+		chunk_lengths,
+		cycles_per_byte(data[1], eightkb),
+		'8kb',
+		max_x_label_eightkb)
+	draw_graph_only_aes(
+		ax3,
+		chunk_lengths,
+		cycles_per_byte(data[2], fifteenkb),
+		'15kb',
+		max_x_label_fifteenkb)
+	draw_graph_only_aes(
+		ax4,
+		chunk_lengths,
+		cycles_per_byte(data[3], fiftykb),
+		'50kb',
+		max_x_label_fiftykb)
+
+	plt.tight_layout(pad=1, w_pad=1, h_pad=1, rect=[0, 0, 1, 0.97])
+	plt.show()
+
 if __name__ == '__main__':
 
 	parse_logs()
 
 	#do_graphs_grid('decrypt')
-	do_graphs_grid('encrypt')
+	#do_graphs_grid('encrypt')
+	#do_graphs_grid_only_aes('decrypt')
+	do_graphs_grid_only_aes('encrypt')
